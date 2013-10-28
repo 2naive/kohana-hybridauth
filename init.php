8ba99a2a03baedde63bb77993bef2f61fc440610
2naive <to.naive@gmail.com>
2013-10-29 03:47:23 +0400

    POC

--- init.php
+++ init.php
@@ -5,3 +5,21 @@
  */
 require_once(dirname(__FILE__).'/vendor/hybridauth/hybridauth/Hybrid/Auth.php');
 
+define('HYBRIDAUTH_PATH', realpath(__DIR__.'/vendor/hybridauth/') . DIRECTORY_SEPARATOR);
+
+if (Kohana::$environment === Kohana::DEVELOPMENT)
+{
+    Route::set('hybridauth-install', 'hybridauth/install')
+    ->defaults(array(
+        'directory' => 'HybridAuth',
+        'controller' => 'Index',
+        'action' => 'install',
+    ));
+}
+
+Route::set('hybridauth-endpoint', 'loginauth')
+    ->defaults(array(
+        'directory' => 'HybridAuth',
+        'controller' => 'Index',
+        'action' => 'endpoint',
+    ));
